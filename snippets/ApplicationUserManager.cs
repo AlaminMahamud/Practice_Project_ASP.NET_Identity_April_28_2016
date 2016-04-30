2be34e96291ebbadcf9c3a91bc0a17e75d532c53
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-04-30 14:20:24 +0600

    Create ApplicationUserManager.cs

---
+++ snippets/ApplicationUserManager.cs
@@ -0,0 +1,23 @@
+// Create 
+  // create a user manager instance
+  // validation logic for usernames and passwords
+  // confgure user lockout
+  // two-factor authentication providers
+  // two-factor auth remember
+  // return manager
+
+// add user to role
+  // create userrolestore
+  // find user object by id
+    // exception handling
+  // user roles collection
+  // iterate user roles and add every bit of it
+  //update
+  
+// remove user to role
+  // create user role store instance
+  // find user obj by id
+    // exception handling
+  // user roles collection
+  // iterate user roles and add every bit of it
+  // update
